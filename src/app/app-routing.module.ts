d03582491290db17ed9cfcf59e6f2903b1877dd2
Nuc1earDragon <1nuclear1@gmail.com>
2019-07-15 02:22:09 +0300

    head

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { TanksComponent } from './tanks/tanks.component';
+import { AboutComponent } from './about/about.component';
+import { EducationComponent } from './education/education.component';
+import { SkillsComponent  } from './skills/skills.component';
+import { ContactsComponent } from './contacts/contacts.component';
 
 
 const routes: Routes = [
-  { path: 'tanks', component: TanksComponent }
+  { path: 'tanks', component: TanksComponent },
+  { path: 'about', component: AboutComponent },
+  { path: 'education', component: EducationComponent },
+  { path: 'skills', component: SkillsComponent },
+  { path: 'contacts', component: ContactsComponent }
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,21 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TanksComponent } from './tanks/tanks.component';
+import { HeadComponent } from './head/head.component';
+import { ContactsComponent } from './contacts/contacts.component';
+import { AboutComponent } from './about/about.component';
+import { SkillsComponent } from './skills/skills.component';
+import { EducationComponent } from './education/education.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TanksComponent
+    TanksComponent,
+    HeadComponent,
+    ContactsComponent,
+    AboutComponent,
+    SkillsComponent,
+    EducationComponent
   ],
   imports: [
     BrowserModule,
